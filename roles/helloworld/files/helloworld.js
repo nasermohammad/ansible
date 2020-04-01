75c8b4ef3d98b268e3b484f67b9e284476f3d802
root <root@Naser.localdomain>
2020-04-01 10:53:58 -0500

    Adding role for nodejs and helloworld

---
+++ roles/helloworld/files/helloworld.js
@@ -0,0 +1,16 @@
+--2020-03-31 15:13:16--  http://bit.ly/2vESNuc
+Resolving bit.ly (bit.ly)... 67.199.248.10, 67.199.248.11
+Connecting to bit.ly (bit.ly)|67.199.248.10|:80... connected.
+HTTP request sent, awaiting response... 301 Moved Permanently
+Location: https://raw.githubusercontent.com/EffectiveDevOpsWithAWS/code-by-chapter/master/Chapter02/helloworld.js [following]
+--2020-03-31 15:13:16--  https://raw.githubusercontent.com/EffectiveDevOpsWithAWS/code-by-chapter/master/Chapter02/helloworld.js
+Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 151.101.112.133
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|151.101.112.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 384 [text/plain]
+Saving to: ‘2vESNuc’
+
+     0K                                                       100% 7.17M=0s
+
+2020-03-31 15:13:36 (7.17 MB/s) - ‘2vESNuc’ saved [384/384]
+
